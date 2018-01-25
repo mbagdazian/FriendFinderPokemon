c6b5e4f8ae555d1a1850d253d0583ca1cbd6b033
ewok1138 <mbagdazian90@gmail.com>
2018-01-24 21:24:53 -0800

    HW assigment

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+var friends = [
+
+	{
+		name: "Pikachu",
+		photo: "https://bulbapedia.bulbagarden.net/wiki/File:025Pikachu.png",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"4",
+			"5",
+			"1",
+			"2",
+			"5",
+			"4",
+			"1"
+
+		]
+	},
+	{
+		name: "Charmander",
+		photo: "https://bulbapedia.bulbagarden.net/wiki/File:004Charmander.png",
+		scores: [
+			"4",
+			"2",
+			"4",
+			"3",
+			"3",
+			"2",
+			"4",
+			"5",
+			"4",
+			"3"
+
+		]
+	},
+	{
+		name: "Bulbasaur",
+		photo: "https://bulbapedia.bulbagarden.net/wiki/File:001Bulbasaur.png",
+		scores: [
+			"1",
+			"1",
+			"1",
+			"4",
+			"5",
+			"2",
+			"2",
+			"4",
+			"4",
+			"5"
+
+		]
+	}
+
+];
+
+module.exports = friends;
